584d93e65b10db1dca01858f819d100148c0137b
Steffen Müller <smueller@cpan.org>
2006-05-23 15:29:09 +0000

    Import of SMUELLER/Module-ScanDeps-0.60 from CPAN.
    gitpan-cpan-distribution: Module-ScanDeps
    gitpan-cpan-version:      0.60
    gitpan-cpan-path:         SMUELLER/Module-ScanDeps-0.60.tar.gz
    gitpan-cpan-author:       SMUELLER
    gitpan-cpan-maturity:     released

--- lib/Module/ScanDeps.pm
+++ lib/Module/ScanDeps.pm
@@ -4,7 +4,7 @@ use 5.004;
 use strict;
 use vars qw( $VERSION @EXPORT @EXPORT_OK $CurrentPackage );
 
-$VERSION   = '0.59';
+$VERSION   = '0.60';
 @EXPORT    = qw( scan_deps scan_deps_runtime );
 @EXPORT_OK = qw( scan_line scan_chunk add_deps scan_deps_runtime );
 
@@ -574,7 +574,7 @@ sub scan_chunk {
 
         return [ 'encoding.pm',
             map { _find_encoding($_) }
-              grep { length and !/^q[qw]?$/ } split(/[^\w:]+/, $1) ]
+              grep { length and !/^q[qw]?$/ } split(/[^\w:-]+/, $1) ]
           if /^\s* use \s+ encoding \s+ (.*)/sx;
 
         return $1 if /(?:^|\s)(?:use|no|require)\s+([\w:\.\-\\\/\"\']+)/;
